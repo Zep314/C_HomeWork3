88416585baf789fbcc1b7acb04151f0217e9acaf
Dmitry Mordanov <Mordanov314@gmail.com>
2022-03-04 22:09:18 +0300

    task 23 completed

--- task21/Program.cs
+++ task21/Program.cs
@@ -2,22 +2,22 @@
 // и находит расстояние между ними в 3D пространстве.
 using System;
 
-Console.WriteLine("Введите координату X1 первой точки");
+Console.Write("Введите координату X1 первой точки: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y1 первой точки");
+Console.Write("Введите координату Y1 первой точки: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Z1 первой точки");
+Console.Write("Введите координату Z1 первой точки: ");
 int z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату X2 второй точки");
+Console.Write("Введите координату X2 второй точки: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y2 второй точки");
+Console.Write("Введите координату Y2 второй точки: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Z2 второй точки");
+Console.Write("Введите координату Z2 второй точки: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
 double P = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)); // вычисление
---
+++ task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+using System;
+
+Console.Write("Введите число N, больше 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N<1)
+{
+    Console.WriteLine("Введены неверные данные.");
+}
+else
+{
+    for(int i=1; i<=N; i++)
+    {
+        Console.WriteLine($"Куб числа {i} равен {Math.Pow(i,3)}");
+    }
+}
